f72d60342d3c4c594b29e6857a4ba11cbe78dc30
RodrigoMoreno <rroderickk@gmail.com>
2021-12-11 02:54:00 -0500

    first original

---
+++ README.md
@@ -0,0 +1,25 @@
+# The React component with vanilla HTML5 canvas (using hooks)
+
+![screenshot](screenshot.gif)
+
+The text falls in columns each of 20px wide.
+
+In order to get the start position of every column we will create an array.
+
+```javascript
+const columns = Math.floor(w / 20) + 1;
+const startPositions = Array.from({ length: columns }).map((_, i) => ({
+  x: i * 20,
+  y: 0,
+}));
+```
+
+In each frame of the animation, a single random character will be put at the end of each column. Initially the end (y coordinate) of each column is at 0.
+
+In each frame we render a semi transparent black rectangle on top of the previous frame, so that the characters rendered in previous frames can look progressively dimmed.
+
+Also we randomly reset some columns to start from the top again.
+
+For the rest of the columns the y coordinate will be moved 20px down, so that in next frame a new character appears below the current one.
+
+# cambiando de branch cambias el efecto.
---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useRef, useState, useEffect } from "react";
+import styled from "styled-components";
+import Matrix from "./Matrix";
+
+function App() {
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const containerRef = useRef(null);
+  useEffect(() => {
+    const container = containerRef.current;
+    setDimensions({
+      width: container.offsetWidth,
+      height: container.offsetHeight,
+    });
+
+    const handleWindowResize = () => {
+      setDimensions({
+        width: container.offsetWidth,
+        height: container.offsetHeight,
+      });
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, [containerRef]);
+  return (
+    <Main ref={containerRef}>
+      <Matrix width={dimensions.width} height={dimensions.height}></Matrix>
+    </Main>
+
+  );
+}
+
+export default App;
+
+const Main = styled.div`
+  width: 100vw;
+  height: 100vh; 
+`;
